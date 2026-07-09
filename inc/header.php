a3e9127d6157c510f1d3e5acb9528ffbf0cca77d
Hans Christian <hansmadume@gmail.com>
2026-07-09 16:44:11 +0800

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Sofia&effect=fire">
+</head>
+
+<body>
